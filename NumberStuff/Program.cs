fee220da95f1e331a3cb89042fc94bbbb2488eb1
christensenjoshua <christensen.joshua@outlook.com>
2018-09-26 19:00:37 -0600

    Working basically

--- NumberStuff/Program.cs
+++ NumberStuff/Program.cs
@@ -10,6 +10,31 @@ namespace NumberStuff
     {
         static void Main(string[] args)
         {
+            bool done = false;
+            int number = 1234;
+            int numIterations = 0;
+            while (!done)
+            {
+                numIterations++;
+                var largeS = number.ToString().ToCharArray().OrderByDescending(d => d);
+                var smallS = number.ToString().ToCharArray().OrderBy(d => d);
+                int.TryParse(String.Join("",largeS), out int large);
+                int.TryParse(String.Join("",smallS), out int small);
+                number = large - small;
+                if(number == 6174)
+                {
+                    Console.WriteLine("You did it in " + numIterations + " iterations!");
+                    done = true;
+                    continue;
+                }
+                if(numIterations > 8)
+                {
+                    Console.WriteLine("You failed to get the number in 8 iterations or less!");
+                    done = true;
+                    continue;
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
